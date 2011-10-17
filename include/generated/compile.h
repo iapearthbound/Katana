4aae3e2abed47d9857c759d56fa91335261e2fbf
Shankar <mathkid95@gmail.com>
2011-10-16 22:00:02 -0500

    Add Deep Idle by Ezekeel
    Conflicts:
    
    	arch/arm/mach-s5pv210/include/.DS_Store
    	arch/arm/mach-s5pv210/include/mach/.DS_Store
    	drivers/misc/.DS_Store
    	include/config/auto.conf
    	include/config/kernel.release
    	include/config/tristate.conf
    	include/generated/autoconf.h
    	include/generated/compile.h
    	include/generated/utsrelease.h

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Wed Oct 12 18:41:01 CDT 2011"
+#define LINUX_COMPILE_TIME "18:41:01"
+#define LINUX_COMPILE_BY "shankar"
+#define LINUX_COMPILE_HOST "Matr1x.local"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.14-ck1-bfs-g3a634d0-dirty"
