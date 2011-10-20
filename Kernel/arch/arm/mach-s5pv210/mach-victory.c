2de4326b8b5ad7ae7721db782d26e27dc0bd119e
Scott <Tortel1210@gmail.com>
2011-10-19 22:47:25 -0400

    Upped framerate cap to 72 - Thanks UberPinguin and Rodderik

--- Kernel/arch/arm/mach-s5pv210/mach-victory.c
+++ Kernel/arch/arm/mach-s5pv210/mach-victory.c
@@ -321,7 +321,7 @@ static struct s3cfb_lcd nt35580 = {
 	.p_width = 52,
 	.p_height = 86,
 	.bpp = 24,
-	.freq = 60,
+	.freq = 72,
 	.timing = {
 		.h_fp = 10,
 		.h_bp = 20,
@@ -1151,7 +1151,7 @@ static struct s3cfb_lcd s6e63m0 = {
 	.p_width = 52,
 	.p_height = 86,
 	.bpp = 24,
-	.freq = 60,
+	.freq = 72,
 
 	.timing = {
 		.h_fp = 16,
