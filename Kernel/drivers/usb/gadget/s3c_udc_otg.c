e31d463ec9b31906946d03f786209b3e821eedfc
Rodderik <usmccool@gmail.com>
2011-10-25 18:25:33 -0400

    Add 600MHz, 1.1GHz, 1.3GHz, & 1.4GHz

--- Kernel/drivers/usb/gadget/s3c_udc_otg.c
+++ Kernel/drivers/usb/gadget/s3c_udc_otg.c
@@ -438,7 +438,7 @@ void s5p_set_otg_dvfs(int enable)
 	}
 =======
   if (enable) {
-                s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L2); //800MHz lock
+                s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L5); //800MHz lock
         } else {
                 s5pv210_unlock_dvfs_high_level(DVFS_LOCK_TOKEN_8);
         }
@@ -463,7 +463,7 @@ int s3c_vbus_enable(struct usb_gadget *gadget, int enable)
 =======
 	 if (enable) {
   		dev_info(&gadget->dev, "USB udc %d,%d lock\n", dev->udc_enabled, enable);
-  		s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L2); //800MHz lock
+  		s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L5); //800MHz lock
  	} else {
   		dev_info(&gadget->dev, "USB udc %d,%d unlock\n", dev->udc_enabled, enable);
   		s5pv210_unlock_dvfs_high_level(DVFS_LOCK_TOKEN_8);
